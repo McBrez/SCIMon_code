23cfbd02404ef3551767076f0d0a1dfa63b7946f
David Freismuth <e1326907@student.tuwien.ac.at>
2022-11-22 13:47:43 +0100

    Intermediate commit.

---
+++ src/_shared_/include/Devices/elveflow_ob1/device_ob1.hpp
@@ -0,0 +1,13 @@
+#ifndef DEVICE_OB1_HPP
+#define DEiCE_OB1_HPP
+
+#include <device.hpp>
+
+namespace Devices {
+class DeviceOb1 : public Device {
+public:
+  DeviceOb1();
+};
+} // namespace Devices
+
+#endif
