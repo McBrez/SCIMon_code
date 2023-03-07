8f7b2e514868a9bd1b80b243796b78ef49ab8fc4
Freismuth David <e1326907@student.tuwien.ac.at>
2023-03-07 07:16:10 +0100

    Intermediate commit.

--- src/_shared_/include/Devices/isx3/com_interface_codec.hpp
+++ src/_shared_/include/Devices/isx3/com_interface_codec.hpp
@@ -10,12 +10,29 @@
 using namespace Messages;
 
 namespace Devices {
+
+/**
+ * @brief Identifies measurements modes as used by the init measurement command.
+ */
+enum MeasurementMode {
+  MEASUREMENT_MODE_INVALID = 0xFF,
+  MEASUREMENT_MODE_EFP = 0x00,
+  MEASUREMENT_MODE_EFP_STIMULATION = 0x01,
+  MEASUREMENT_MODE_IMPEDANCE_SPECTROSCOPY = 0x02,
+  MEASUREMENT_MODE_FULL_RANGE_IMPEDANCE_SPECTROSCOPY = 0x03
+};
+
 /**
  * @brief Encapsulates a Codec that adheres to the COM interface defined by
  * Sciospec.
  */
 class ComInterfaceCodec {
 public:
+  static const unsigned char CmdTagAck = 0x18;
+  static const unsigned char CmdTagResetSystem = 0xA1;
+  static const unsigned char CmdTagGetDeviceId = 0xD1;
+  static const unsigned char CmdTagInitMeasurement = 0xB4;
+
   /**
    * @brief Construct a new Com Interface Codec object
    */
@@ -27,18 +44,42 @@ public:
    */
   std::vector<unsigned char> buildCmdXYZ();
 
-/**
- * @brief Decodes the given raw data and parses it to a device message.
- * @param bytes The raw data that shall be parsed.
- * @return shared_ptr<DeviceMessage> The parsed message.
- */
+  /**
+   * @brief Builds a Reset System command.
+   * @return An Reset System command.
+   */
+  std::vector<unsigned char> buildCmdResetSystem();
+
+  /**
+   * @brief Builds a Get Device ID command.
+   * @return An Get Device ID command.
+   */
+  std::vector<unsigned char> buildCmdGetDeviceId();
+
+  /**
+   * @brief Builds an Init Measurement command from the given arguments.
+   * @param readWrite Specifies whether the measurement mode shall be read from
+   * the device, or shall be written to it. TRUE corresponds to read, FALSE to
+   * write.
+   * @param measurementMode The measurement mode that shall be set. Ignored if
+   * readWrite == TRUE.
+   * @return An Init Measurement command.
+   */
+  std::vector<unsigned char>
+  buildCmdInitMeasurement(bool readWrite, MeasurementMode measurementMode);
+
+  /**
+   * @brief Decodes the given raw data and parses it to a device message.
+   * @param bytes The raw data that shall be parsed.
+   * @return shared_ptr<DeviceMessage> The parsed message.
+   */
   shared_ptr<DeviceMessage> decodeMessage(std::vector<unsigned char> bytes);
 
-/**
- * @brief Encodes the given device message to a byte vector.
- * @param message The message that shall be parsed.
- * @return The encoded byte vector.
- */
+  /**
+   * @brief Encodes the given device message to a byte vector.
+   * @param message The message that shall be parsed.
+   * @return The encoded byte vector.
+   */
   std::vector<unsigned char> encodeMessage(shared_ptr<DeviceMessage> message);
 };
 } // namespace Devices
--- src/_shared_/src/Devices/isx3/com_interface_codec.cpp
+++ src/_shared_/src/Devices/isx3/com_interface_codec.cpp
@@ -9,6 +9,30 @@ std::vector<unsigned char> ComInterfaceCodec::buildCmdXYZ() {
   return std::vector<unsigned char>();
 }
 
+std::vector<unsigned char> ComInterfaceCodec::buildCmdResetSystem() {
+  return std::vector<unsigned char>(
+      {CmdTagResetSystem, 0x00, CmdTagResetSystem});
+}
+
+std::vector<unsigned char> ComInterfaceCodec::buildCmdGetDeviceId() {
+  return std::vector<unsigned char>(
+      {CmdTagGetDeviceId, 0x00, CmdTagGetDeviceId});
+}
+
+std::vector<unsigned char>
+ComInterfaceCodec::buildCmdInitMeasurement(bool readWrite,
+                                           MeasurementMode measurementMode) {
+
+  if (readWrite) {
+    return std::vector<unsigned char>(
+        {CmdTagInitMeasurement, 0x01, 0x80, CmdTagInitMeasurement});
+  } else {
+    return std::vector<unsigned char>(
+        {CmdTagInitMeasurement, 0x02, 0x00,
+         static_cast<unsigned char>(measurementMode), CmdTagInitMeasurement});
+  }
+}
+
 shared_ptr<DeviceMessage>
 ComInterfaceCodec::decodeMessage(std::vector<unsigned char> bytes) {
   return shared_ptr<DeviceMessage>();
