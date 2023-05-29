1bd96db5a04f78f93a8a3150d107fe43427a15fe
Freismuth David <e1326907@student.tuwien.ac.at>
2023-05-29 23:46:30 +0200

    Intermediate commit.

--- src/_shared_/src/Devices/elveflow_ob1/ob1_payload_decoder.cpp
+++ src/_shared_/src/Devices/elveflow_ob1/ob1_payload_decoder.cpp
@@ -1,12 +1,37 @@
 // Project includes
+#include <common.hpp>
+#include <ob1_conf_payload.hpp>
+#include <ob1_init_payload.hpp>
 #include <ob1_payload_decoder.hpp>
+
+// 3rd party includes
+#include <flatbuffers/flatbuffers.h>
+
+// Generated includes
+#include <ob1_init_payload_generated.h>
 
 using namespace Devices;
 
 InitPayload *
 Ob1PayloadDecoder::decodeInitPayload(const vector<unsigned char> &data,
                                      int magicNumber) {
-  return nullptr;
+  const unsigned char *buffer = data.data();
+
+  if (MAGIC_NUMBER_OB1_INIT_PAYLOAD == magicNumber) {
+    const Serialization::Devices::Ob1InitPayloadT *ob1Payload =
+        Serialization::Devices::GetOb1InitPayload(buffer)->UnPack();
+
+    string deviceName = ob1Payload->deviceName;
+    ChannelConfiguration channelConfiguration{
+        ob1Payload->channelconfiguration->channel1(),
+        ob1Payload->channelconfiguration->channel2(),
+        ob1Payload->channelconfiguration->channel3(),
+        ob1Payload->channelconfiguration->channel4()};
+
+    return new Ob1InitPayload(deviceName, channelConfiguration);
+  } else {
+    return nullptr;
+  }
 }
 
 ConfigurationPayload *
